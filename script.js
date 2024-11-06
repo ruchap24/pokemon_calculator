a24f071ddf43ddd1e39c0f26951815fc2fbad6bb
ruchap24 <patilrucha991@gmail.com>
2024-11-06 19:25:14 +0530

    style sheet & script modified

---
+++ script.js
@@ -0,0 +1,12 @@
+const typeChart = {
+    bug: { fighting: 0.5, grass: 0.5, ground: 0.5, fire: 2, flying: 2, rock: 2 },
+    dark: { dark: 0.5, ghost: 0.5, psychic: 0.5, bug: 2, fairy: 2, fighting: 2 },
+    dragon: {
+      electric: 0.5,
+      fire: 0.5,
+      grass: 0.5,
+      water: 0.5,
+      dragon: 2,
+      fairy: 2,
+      ice: 2,
+    },
